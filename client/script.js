89e3b8fb2f91736e0d5e9402110bcff087c45a08
shanni6 <240686886@qq.com>
2023-04-06 14:11:15 +0800

    Apr6 Commit

--- client/script.js
+++ client/script.js
@@ -41,9 +41,9 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
     return `
-   <div className="wrapper ${isAi && "ai"}">
+   <div class="wrapper ${isAi && "ai"}">
    <div class="chat">
-   <div className="profile">
+   <div class="profile">
    <img src="${isAi ? bot : user}"
    alt="${isAi ? "bot" : "user"}"
    />
@@ -72,11 +72,31 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId);
 
     loader(messageDiv);
+    const response = await fetch("http://localhost:3000",{
+      method:'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },body:JSON.stringify
+      ({
+        prompt:data.get('prompt')
+      })
+    });
+clearInterval(loadInterval)
+messageDiv.innerHTML = ''
+if(response.ok){
+  const data = await response.json()
+  const parsedData = data.bot.trim()
+  typeText(messageDiv,parsedData);
+}else{
+  const err = await response.text()
+  messageDiv.innerHTML = "Something went wrong"
+  alert(err)
+}
 };
 
-form.addEventListener('submit',handleSubmit);
-form.addEventListener('keyup',(e) => {
-  if(e.keyCode === 13){
-    handleSubmit(e)
-  }
-})
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e);
+    }
+});
